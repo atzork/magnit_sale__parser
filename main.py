bdd466d2a03ecadcc7c2d7f4cdc6c1c542f49193
zork <atzork@gmail.com>
2022-04-11 20:39:23 +0300

    done

--- main.py
+++ main.py
@@ -1,16 +1,66 @@
-# This is a sample Python script.
+import csv
+import datetime
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
 
-# Press the green button in the gutter to run the script.
+def collect_data(city_code='2398'):
+    cur_time = datetime.datetime.now().strftime('%d_%m_%Y_%H_%M')
+    ua = UserAgent()
+    headers = {
+        'User-Agent': ua.random,
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+
+    }
+
+    responce = requests.get(url='https://prom.ua/Avtozapchasti', headers=headers)
+
+    # with open(f'index.html', 'w', encoding='utf-8') as file:
+    #     file.write(responce.text)
+
+    # with open(f'index.html', 'r', encoding='utf-8') as file:
+    #     src = file.read()
+
+    soup = BeautifulSoup(responce.text, 'lxml')
+    cards = soup.findAll('div', class_='js-productad')
+
+    output_file = f'{cur_time}.csv'
+
+    with open(output_file, 'w', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow((
+            'Продукт',
+            'Цена',
+            'Компания',
+            'Город',
+            'Рейтинг'
+        ))
+
+    for card in cards:
+        title = card.select('span[data-qaid="product_name"]')[0].text.strip()
+        price = card.select('span[data-qaid="product_price"]')[0].text.strip().replace('&nbsp;', ' ')
+        city = card.select('span[data-qaid="region_title"]')[0].text.strip()
+        rate = card.select('span[data-qaid="short_company_rating"]')[0].text.strip()
+        company = card.select('a[data-qaid="company_name"]')[0].text.strip()
+        print(title + '\n' + price + '\n' + city + '\n' + company + '\n' + rate)
+
+        with open(output_file, 'a', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow((
+                title,
+                price,
+                company,
+                city,
+                rate
+            ))
+
+        print(f'{output_file} is ready')
+
+def main():
+    collect_data()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
